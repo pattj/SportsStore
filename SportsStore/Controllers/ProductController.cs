c29f79eb2ed4ae6e5ea50d579073d94106f4d5d8
Pattaphol Jirasetsakul <pattaphol_j@hotmail.com>
2018-08-04 21:29:10 -0700

    Add project files.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        /*When MVC needs to create a ProductController class to handle an 
+        HTTP request, it will inspect the constructor to see that it requires
+        an object that implements the IProductRepository interface. 
+        MVC will then check startup.cs to see what implementation class would be used
+        An object of that specified class would be created everytime for use with 
+        the constructor. 
+        
+        This allows the controller constructor to access the data repository
+        thru the IProductRepository interface without having any need to know which 
+        implementation class has been configured.
+
+        */
+
+        public ProductController (IProductRepository repo)
+        {
+            repository = repo; 
+        }
+
+        public ViewResult List() =>View(repository.Products);
+        
+    }
+}
---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace SportsStore.Models
+
+{
+    //For providing access to EFC's underlying functionality
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        //For accessing the 'Products' object in the DB.
+        public DbSet<Product> Products { get; set; }
+    }
+}
